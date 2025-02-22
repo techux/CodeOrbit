699959f5af0f3ab62a378b2bd0fa73becd1eb5c5
techux <connect.world12345@gmail.com>
2025-02-22 12:14:49 +0530

    seperate all the platform specific functionality on service files

--- controllers/stats.controller.js
+++ controllers/stats.controller.js
@@ -1,7 +1,11 @@
 const User = require("../models/user.model");
-const { leetcode, gfg, code360 } = require("../utils/getData");
 
-const leetcodeController = async (req, res) => {
+const leetcode = require("../services/leetcode.service");
+const gfg = require("../services/gfg.service");
+const code360 = require("../services/code360.service");
+
+// leetcode question submission stats
+const lcQuestionController = async (req, res) => {
     try {
         const username = (await User.findById(req.user.id).select("leetcode")).leetcode;
         if (!username){
@@ -10,13 +14,13 @@ const leetcodeController = async (req, res) => {
                 message: "Please set the Leetcode username first"
             });
         }
-        const data = await leetcode(username);
+        const data = await leetcode.questions(username);
         return res.status(200).json({
             status: "ok",
             data: data
         })
     } catch (error) {
-        console.error(`Error in leetcodeController : ${error.stack || error.message}`);
+        console.error(`Error in lcQuestionController : ${error.stack || error.message}`);
         return res.status(500).json({
             status:"error", 
             message: "Internal Server Error" 
@@ -25,7 +29,8 @@ const leetcodeController = async (req, res) => {
 }
 
 
-const gfgController = async (req, res) => {
+// geeksforgeeks submissions only
+const gfgSubmissionController = async (req, res) => {
     try {
         const username = (await User.findById(req.user.id).select("gfg")).gfg;
         if (!username){
@@ -34,13 +39,13 @@ const gfgController = async (req, res) => {
                 message: "Please set the GeeksForGeeks username first"
             });
         }
-        const data = await gfg(username);
+        const data = await gfg.submissions(username);
         return res.status(200).json({
             status: "ok",
             data: data
         })
     } catch (error) {
-        console.error(`Error in gfgController : ${error.stack || error.message}`);
+        console.error(`Error in gfgSubmissionController : ${error.stack || error.message}`);
         return res.status(500).json({
             status:"error", 
             message: "Internal Server Error" 
@@ -58,7 +63,7 @@ const code360Controller = async (req, res) => {
                 message: "Please set the Naukri Code360 username first"
             });
         }
-        const data = await code360(username);
+        const data = await code360.questions(username);
         return res.status(200).json({
             status: "ok",
             data: data
@@ -73,7 +78,7 @@ const code360Controller = async (req, res) => {
 }
 
 module.exports = {
-    leetcodeController,
-    gfgController,
+    lcQuestionController,
+    gfgSubmissionController,
     code360Controller
 }
--- routes/stats.route.js
+++ routes/stats.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const { leetcodeController, gfgController, code360Controller } = require("../controllers/stats.controller");
+const { lcQuestionController, gfgSubmissionController, code360Controller } = require("../controllers/stats.controller");
 
 
 const router = express.Router();
 
-router.get("/leetcode", leetcodeController);
-router.get("/gfg", gfgController);
+router.get("/leetcode", lcQuestionController);
+router.get("/gfg", gfgSubmissionController);
 router.get("/code360", code360Controller);
 
 module.exports = router;
