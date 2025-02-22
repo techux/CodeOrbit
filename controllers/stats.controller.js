3cf818c5a5517eeb915e26b74c6baf27a35fe673
techux <connect.world12345@gmail.com>
2025-02-22 21:24:18 +0530

    Added codeforces and hackerrank routes

--- controllers/stats.controller.js
+++ controllers/stats.controller.js
@@ -3,6 +3,7 @@ const User = require("../models/user.model");
 const leetcode = require("../services/leetcode.service");
 const gfg = require("../services/gfg.service");
 const code360 = require("../services/code360.service");
+const codeforces = require("../services/codeforces.service")
 
 // leetcode question submission stats
 const lcQuestionController = async (req, res) => {
@@ -77,8 +78,34 @@ const code360Controller = async (req, res) => {
     }
 }
 
+
+// codeforces profile
+const codeforcesController = async (req, res) => {
+    try {
+        const username = (await User.findById(req.user.id).select("codeforces")).codeforces;        
+        if (!username){
+            return res.status(404).json({
+                status: "error",
+                message: "Please set the CodeForces username first"
+            });
+        }
+        const data = await codeforces.questionsCount(username);
+        return res.status(200).json({
+            status: "ok",
+            data: data
+        })
+    } catch (error) {
+        console.error(`Error in codeforcesController : ${error.stack || error.message}`);
+        return res.status(500).json({
+            status:"error", 
+            message: "Internal Server Error" 
+        });
+    }
+}
+
 module.exports = {
     lcQuestionController,
     gfgSubmissionController,
-    code360Controller
+    code360Controller,
+    codeforcesController
 }
